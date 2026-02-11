73ec079538875dbddb071e6f6c57afc968d40b42
Chisom Mkparu <chisom.mkparu@vfdtech.ng>
2026-02-11 23:45:45 +0100

    update template and fix vulnerabilities

--- plopfile.js
+++ plopfile.js
@@ -1,68 +1,104 @@
 const dayjs = require("dayjs");
 const { v4: uuidv4 } = require("uuid");
+const pluralize = require("pluralize");
 
 module.exports = function (plop) {
-    // Helpers for text formatting
-    plop.setHelper("capitalize", (text) => text.charAt(0).toUpperCase() + text.slice(1));
-    plop.setHelper("uppercase", (text) => text.toUpperCase()); // Add uppercase helper
-    plop.setHelper("timestamp", () => dayjs().format("YYYY-MM-DD-HH-mm-ss"));
+
+    /* -------------------------------------------------------------------------- */
+    /*                                HELPERS                                     */
+    /* -------------------------------------------------------------------------- */
+
+    plop.setHelper("capitalize", (text) =>
+        pluralize.singular(text).charAt(0).toUpperCase() +
+        pluralize.singular(text).slice(1)
+    );
+
+    plop.setHelper("uppercase", (text) => text.toUpperCase());
+
+    plop.setHelper("plural", (text) => pluralize(text));
+
+    plop.setHelper("singular", (text) => pluralize.singular(text));
+
+    plop.setHelper("capitalizePlural", (text) =>
+        pluralize(text).charAt(0).toUpperCase() +
+        pluralize(text).slice(1)
+    );
+
+    plop.setHelper("capitalizeSingular", (text) =>
+        pluralize.singular(text).charAt(0).toUpperCase() +
+        pluralize.singular(text).slice(1)
+    );
+
+    plop.setHelper("timestamp", () =>
+        dayjs().format("YYYY-MM-DD-HH-mm-ss")
+    );
+
+    /* -------------------------------------------------------------------------- */
+    /*                             SERVICE GENERATOR                              */
+    /* -------------------------------------------------------------------------- */
 
     plop.setGenerator("service", {
         description: "Generate a new service structure",
+
         prompts: [
             {
                 type: "input",
                 name: "service",
-                message: "Enter the service name:",
+                message: "Enter the service name (singular):",
             },
         ],
+
         actions: [
             {
                 type: "add",
-                path: "src/api/models/{{service}}/index.ts",
+                path: "src/api/models/{{singular service}}/index.ts",
                 templateFile: "templates/model.hbs",
             },
             {
                 type: "add",
-                path: "src/api/models/{{service}}/I{{capitalize service}}.ts",
+                path: "src/api/models/{{singular service}}/I{{capitalizeSingular service}}.ts",
                 templateFile: "templates/modelInterface.hbs",
             },
             {
                 type: "add",
-                path: "src/migrations/{{timestamp}}-create-{{service}}.js",
+                path: "src/migrations/{{timestamp}}-create-{{plural service}}.js",
                 templateFile: "templates/modelMigrations.hbs",
             },
             {
                 type: "add",
-                path: "src/api/controllers/{{service}}/index.ts",
+                path: "src/api/controllers/{{singular service}}/index.ts",
                 templateFile: "templates/controller.hbs",
             },
             {
                 type: "add",
-                path: "src/api/services/{{service}}/repository/{{service}}.repository.ts",
+                path: "src/api/services/{{singular service}}/repository/{{singular service}}.repository.ts",
                 templateFile: "templates/serviceRepository.hbs",
             },
             {
                 type: "add",
-                path: "src/api/services/{{service}}/dtos/{{service}}.dto.ts",
+                path: "src/api/services/{{singular service}}/dtos/{{singular service}}.dto.ts",
                 templateFile: "templates/serviceDto.hbs",
             },
             {
                 type: "add",
-                path: "src/api/services/{{service}}/index.ts",
+                path: "src/api/services/{{singular service}}/index.ts",
                 templateFile: "templates/serviceIndex.hbs",
             },
             {
                 type: "add",
-                path: "tests/{{service}}.test.ts",
+                path: "tests/{{singular service}}.test.ts",
                 templateFile: "templates/test.hbs",
             },
         ],
     });
 
-    // Seed Generator
+    /* -------------------------------------------------------------------------- */
+    /*                                SEED GENERATOR                              */
+    /* -------------------------------------------------------------------------- */
+
     plop.setGenerator("seed", {
         description: "Generate a new seed file",
+
         prompts: [
             {
                 type: "input",
@@ -88,14 +124,17 @@ module.exports = function (plop) {
                 default: "active",
             },
         ],
+
         actions: [
             {
                 type: "add",
-                path: "src/seeders/{{timestamp}}-seed-{{service}}s.js",
+                path: "src/seeders/{{timestamp}}-seed-{{plural service}}.js",
                 templateFile: "templates/modelSeed.hbs",
+
                 data: {
                     uuid: uuidv4(),
-                    hashedPassword: "28f6a5f5ac65a9adaf5693efbfa7c05e5bff31bafbc4f66063989af6d9f142c0",
+                    hashedPassword:
+                        "28f6a5f5ac65a9adaf5693efbfa7c05e5bff31bafbc4f66063989af6d9f142c0",
                 },
             },
         ],
